4920a8da02e08a4626ae408dd637647a74898667
muhammed ali binici <muhammedalibnc@gmail.com>
2017-02-18 01:27:25 -0500

    Map

--- app/src/main/java/moe/edward/eatwithme/Map.java
+++ app/src/main/java/moe/edward/eatwithme/Map.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import com.ortiz.touch.*;
 
 public class Map extends AppCompatActivity {
 
@@ -12,7 +13,9 @@ public class Map extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        ImageView imageView = (ImageView)findViewById(R.id.image);
-        Animation zoom = AnimationUtils.loadAnimation(this, R.anim.zoom);
+        TouchImageView imageView = (TouchImageView)findViewById(R.id.image);
+//        Animation zoom = AnimationUtils.loadAnimation(this, R.anim.zoom);
+//        imageView.startAnimation(zoom);
+
     }
 }
