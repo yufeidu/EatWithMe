9b27bee8d19ad0ba46f3e836c05d86969f7f4cb4
LeveyLeonhardt <edwarddu1996@gmail.com>
2017-02-18 04:00:34 -0500

    Added text submission

--- app/src/main/java/moe/edward/eatwithme/TopicSelection.java
+++ app/src/main/java/moe/edward/eatwithme/TopicSelection.java
@@ -4,12 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -25,6 +31,23 @@ public class TopicSelection extends AppCompatActivity {
         LinearLayout layout = (LinearLayout)findViewById(R.id.linearLayout_Inside_Topic);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
         updateEntryList(layout);
+        EditText text = (EditText)findViewById(R.id.editText_topic);
+        text.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+
+                if(s.toString().equals("")){
+                    ((Button)findViewById(R.id.button_topic)).setEnabled(false);
+                }else{
+                    ((Button)findViewById(R.id.button_topic)).setEnabled(true);
+                }
+
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+        });
     }
 
     //map data structure: {text,X,Y}
@@ -59,9 +82,10 @@ public class TopicSelection extends AppCompatActivity {
     }
 
     public void onClickButton(View e){
-
+        String text = ((EditText)findViewById(R.id.editText_topic)).getText().toString();
         Intent intent = new Intent(TopicSelection.this, Map.class);
-        intent.putExtra("mode",2);
+        intent.putExtra("mode", 2);
+        intent.putExtra("data", text);
         startActivity(intent);
     }
 }
