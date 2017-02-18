008bee2fdcb5570679d094afef2ffca5af512815
LeveyLeonhardt <edwarddu1996@gmail.com>
2017-02-17 23:19:26 -0500

    Added Activities

---
+++ app/src/main/java/moe/edward/eatwithme/TopicSelection.java
@@ -0,0 +1,13 @@
+package moe.edward.eatwithme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TopicSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topic_selection);
+    }
+}
