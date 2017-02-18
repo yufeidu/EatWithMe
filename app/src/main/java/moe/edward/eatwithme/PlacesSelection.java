43e0d2adb700d2427e530ff9214303dad6b7bb0c
muhammed ali binici <muhammedalibnc@gmail.com>
2017-02-18 00:19:32 -0500

    Places

--- app/src/main/java/moe/edward/eatwithme/PlacesSelection.java
+++ app/src/main/java/moe/edward/eatwithme/PlacesSelection.java
@@ -1,13 +1,28 @@
 package moe.edward.eatwithme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class PlacesSelection extends AppCompatActivity {
+public class PlacesSelection extends AppCompatActivity implements View.OnClickListener{
 
+    private String PLACE = "place";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_places_selection);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        Button button = (Button)findViewById(v.getId());
+
+        Intent intent = new Intent(PlacesSelection.this, TopicSelection.class);
+        intent.putExtra(PLACE, button.getId());
+        startActivity(intent);
+    }
+
 }
