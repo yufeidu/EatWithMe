018782473679273b0cc1d71d8184085964ddcf3b
LeveyLeonhardt <edwarddu1996@gmail.com>
2017-02-17 22:39:58 -0500

    Initial Commit

--- app/src/main/java/moe/edward/eatwithme/LoginActivity.java
+++ app/src/main/java/moe/edward/eatwithme/LoginActivity.java
@@ -51,6 +51,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
     };
+    //Lol
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
